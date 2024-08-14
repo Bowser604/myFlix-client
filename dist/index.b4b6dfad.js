93a9fa6d0603bd47fa68a14c54cce8ca10183ca4
Bowser604 <146279617+Bowser604@users.noreply.github.com>
2024-08-13 18:22:53 -0700

    typo fix

--- dist/index.b4b6dfad.js
+++ dist/index.b4b6dfad.js
@@ -28623,7 +28623,7 @@ const SignupView = ()=>{
                         value: username,
                         onChange: (e)=>setUsername(e.target.value),
                         required: true,
-                        minLength: "3"
+                        minLength: 3
                     }, void 0, false, {
                         fileName: "src/components/signup-view/signup-view.jsx",
                         lineNumber: 42,
--- src/components/signup-view/signup-view.jsx
+++ src/components/signup-view/signup-view.jsx
@@ -44,7 +44,7 @@ export const SignupView = () => {
                     value={username}
                     onChange={(e) => setUsername(e.target.value)}
                     required
-                    minLength="3"
+                    minLength={3}
                 />
             </Form.Group>
             <Form.Group controlId="formPassword">
